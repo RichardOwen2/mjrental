6aeb67cccdeef1bae6982a6652dc7cda0ceb84aa
Richard Owen <richardowen2411@gmail.com>
2024-07-29 21:39:50 +0800

    create layout

---
+++ resources/views/layouts/admin/header.blade.php
@@ -0,0 +1,30 @@
+<div id="kt_app_header" class="app-header d-flex align-items-stretch">
+    <div class="app-container container-fluid d-flex align-items-stretch justify-content-between"
+        id="kt_app_header_container">
+
+        <div class="app-header-wrapper d-flex flex-grow-1 justify-content-around justify-content-lg-between flex-wrap gap-3 mb-6 mb-lg-0"
+            data-kt-swapper="true" data-kt-swapper-mode="{default: 'prepend', lg: 'prepend'}"
+            data-kt-swapper-parent="{default: '#kt_app_content_container', lg: '#kt_app_header_container'}">
+
+            <div class="d-flex align-items-stretch">
+                <div class="app-header-logo d-flex flex-center">
+                    <a href="/">
+                        <img alt="Logo" src="{{ asset('metronic') }}/media/logos/demo-56.svg"
+                            class="h-35px">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="d-flex d-lg-none flex-grow-1 flex-stack gap-4">
+            <button class="btn btn-icon btn-sm btn-active-color-primary ms-n2"
+                id="kt_app_sidebar_mobile_toggle">
+                <i class="ki-outline ki-abstract-14 fs-1"></i> </button>
+
+            <a href="/">
+                <img alt="Logo" src="{{ asset('metronic') }}/media/logos/demo-56.svg"
+                    class="h-30px">
+            </a>
+        </div>
+    </div>
+</div>
---
+++ resources/views/layouts/admin/sidebar.blade.php
@@ -0,0 +1,136 @@
+<div id="kt_app_sidebar" class="app-sidebar" data-kt-drawer="true" data-kt-drawer-name="app-sidebar"
+    data-kt-drawer-activate="{default: true, lg: false}" data-kt-drawer-overlay="true" data-kt-drawer-width="auto"
+    data-kt-drawer-direction="start" data-kt-drawer-toggle="#kt_app_sidebar_mobile_toggle">
+
+    <div class="app-sidebar-menu flex-grow-1 hover-scroll-y scroll-lg-ps my-5" id="kt_aside_menu_wrapper"
+        data-kt-scroll="true" data-kt-scroll-height="auto"
+        data-kt-scroll-dependencies="#kt_app_sidebar_logo, #kt_app_sidebar_footer"
+        data-kt-scroll-wrappers="#kt_app_sidebar_menu" data-kt-scroll-offset="5px" style="height: 264px;">
+        <div id="kt_aside_menu"
+            class="menu menu-rounded menu-column menu-title-gray-600 menu-state-primary menu-state-icon-primary menu-state-bullet-primary menu-arrow-gray-500 fw-semibold fs-6"
+            data-kt-menu="true">
+            <div data-kt-menu-trigger="{default: 'click', lg: 'hover'}" data-kt-menu-placement="right-start"
+                class="menu-item here show py-2">
+                <span class="menu-link menu-center"><span class="menu-icon me-0"><i
+                            class="ki-outline ki-home-2 fs-1"></i></span></span>
+                <div class="menu-sub menu-sub-dropdown px-2 py-4 w-250px mh-75 overflow-auto">
+                    <div class="menu-item">
+                        <div class="menu-content "><span class="menu-section fs-5 fw-bolder ps-1 py-1">Home</span></div>
+                    </div>
+
+                    <div class="menu-item">
+                        <a class="menu-link active" href="/metronic8/demo56/index.html">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">Default</span>
+                        </a>
+                    </div>
+
+                    <div class="menu-item">
+                        <a class="menu-link" href="/metronic8/demo56/dashboards/ecommerce.html">
+                            <span class="menu-bullet">
+                                <span class="bullet bullet-dot"></span>
+                            </span>
+                            <span class="menu-title">eCommerce</span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="d-flex flex-column flex-center pb-4 pb-lg-8" id="kt_app_sidebar_footer">
+
+        <div class="cursor-pointer symbol symbol-40px" data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
+            data-kt-attach="parent" data-kt-menu-placement="right-end">
+            <img src="{{ asset('metronic') }}/media/avatars/300-7.jpg" alt="user">
+        </div>
+
+        <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-gray-800 menu-state-bg menu-state-color fw-semibold py-4 fs-6 w-275px"
+            data-kt-menu="true">
+            <div class="menu-item px-3">
+                <div class="menu-content d-flex align-items-center px-3">
+                    <div class="symbol symbol-50px me-5">
+                        <img alt="Logo" src="{{ asset('metronic') }}/media/avatars/300-7.jpg">
+                    </div>
+
+                    <div class="d-flex flex-column">
+                        <div class="fw-bold d-flex align-items-center fs-5">
+                            Max Smith <span class="badge badge-light-success fw-bold fs-8 px-2 py-1 ms-2">Pro</span>
+                        </div>
+
+                        <a href="#" class="fw-semibold text-muted text-hover-primary fs-7">
+                            max@kt.com
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="separator my-2"></div>
+
+            <div class="menu-item px-5" data-kt-menu-trigger="{default: 'click', lg: 'hover'}"
+                data-kt-menu-placement="left-start" data-kt-menu-offset="-15px, 0">
+                <a href="#" class="menu-link px-5">
+                    <span class="menu-title position-relative">
+                        Mode
+
+                        <span class="ms-5 position-absolute translate-middle-y top-50 end-0">
+                            <i class="ki-outline ki-night-day theme-light-show fs-2"></i> <i
+                                class="ki-outline ki-moon theme-dark-show fs-2"></i> </span>
+                    </span>
+                </a>
+
+                <!--begin::Menu-->
+                <div class="menu menu-sub menu-sub-dropdown menu-column menu-rounded menu-title-gray-700 menu-icon-gray-500 menu-active-bg menu-state-color fw-semibold py-4 fs-base w-150px"
+                    data-kt-menu="true" data-kt-element="theme-mode-menu">
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3 my-0">
+                        <a href="#" class="menu-link px-3 py-2 active" data-kt-element="mode"
+                            data-kt-value="light">
+                            <span class="menu-icon" data-kt-element="icon">
+                                <i class="ki-outline ki-night-day fs-2"></i> </span>
+                            <span class="menu-title">
+                                Light
+                            </span>
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3 my-0">
+                        <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="dark">
+                            <span class="menu-icon" data-kt-element="icon">
+                                <i class="ki-outline ki-moon fs-2"></i> </span>
+                            <span class="menu-title">
+                                Dark
+                            </span>
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+
+                    <!--begin::Menu item-->
+                    <div class="menu-item px-3 my-0">
+                        <a href="#" class="menu-link px-3 py-2" data-kt-element="mode" data-kt-value="system">
+                            <span class="menu-icon" data-kt-element="icon">
+                                <i class="ki-outline ki-screen fs-2"></i> </span>
+                            <span class="menu-title">
+                                System
+                            </span>
+                        </a>
+                    </div>
+                    <!--end::Menu item-->
+                </div>
+                <!--end::Menu-->
+
+            </div>
+
+            <div class="menu-item px-5">
+                <a href="/" class="menu-link px-5">
+                    Sign Out
+                </a>
+            </div>
+        </div>
+
+    </div>
+</div>
